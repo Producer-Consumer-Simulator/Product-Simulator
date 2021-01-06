72d651ee95536949b0ee2de98620463be75aa7a2
ahmed <ahmed@192.168.1.7>
2021-01-06 22:50:41 +0200

    add services modify models

---
+++ Producer Simulation/src/models/Factory.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Queue;
+public class Factory {
+	
+	private ArrayList<Queue> FactoryQueues = new ArrayList<Queue>();
+	private ArrayList<Machine> FactoryMachines = new ArrayList<Machine>();
+	
+}
