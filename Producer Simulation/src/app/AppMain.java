67f04133097a88b89465429c13ae20bed1804ebf
ahmed <ahmed@192.168.1.7>
2021-01-09 20:51:55 +0200

    remove changes

--- Producer Simulation/src/app/AppMain.java
+++ Producer Simulation/src/app/AppMain.java
@@ -1,13 +1,13 @@
 package app;
 
 import app.GUI.mainGUI;
-import app.GUI.productGui;
+
 
 public class AppMain {
 
 	public static void main(String[] args) {
 
 		new Thread(new mainGUI()).start();
-		new Thread(new productGui()).start();
+		//new Thread(new productGui()).start();
 	}
 }
