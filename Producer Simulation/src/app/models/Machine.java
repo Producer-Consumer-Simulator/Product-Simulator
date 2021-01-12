3cc9e6283a83726ea5fe88fc07026fa9256323bf
Ahmed Ashraf <Ahmed Ashraf@DESKTOP-24MGP8E>
2021-01-12 21:17:26 +0200

    finish

--- Producer Simulation/src/app/models/Machine.java
+++ Producer Simulation/src/app/models/Machine.java
@@ -112,7 +112,7 @@ public class Machine implements Runnable {
 				long t = this.time - (System.currentTimeMillis() - StartTime);
 				while (t > 0) {
 					String ti = Long.toString(t / 1000);
-					//System.out.println(ti);
+					System.out.println(ti);
 					Platform.runLater(() -> {
 						this.guiShape.setText(ti);
 					});
